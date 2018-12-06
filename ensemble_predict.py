13c41f8ce5ee2fad317cc2a58aadd2821522a94d
Lin, Wei-Kai <spreadwyvern@gmail.com>
2018-12-06 12:44:55 +0800

    added CKD stage classification

--- ensemble_predict.py
+++ ensemble_predict.py
@@ -38,6 +38,9 @@ from utils.generator import kidney_Dataset
 from utils.model import freeze_blocks_resnet, CNN
 from utils.img_preprocess import img_process_transform, img_process_PIL
 
+import xgboost as xgb
+import pickle
+
 import argparse
 
 def main():
@@ -47,11 +50,12 @@ def main():
 
     FLAG = parser.parse_args()
 
-    predict_egfr(FLAG)
+    predict_kidney(FLAG)
+
+# def predict_kidney(FLAG):
 
 
-
-def predict_egfr(FLAG):
+def predict_kidney(FLAG):
     os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = FLAG.gpu_id 
     torch.cuda.set_device(0)
@@ -83,43 +87,79 @@ def predict_egfr(FLAG):
                 print("loading trained models...")
                 # create models dict
                 models = {}
-
+                extract_models = {}
+                # model directory 
+                model_dir = 'models/'
                 # model ensemble
                 for i in range(10):    
                     # ResNet-101 in submitted paper
                     model = CNN(drop_rate=0.5).type(dtype)
                     net = torch.nn.DataParallel(model)
-
-                    # model directory 
-                    model_dir = 'models/'
-
                     net.load_state_dict(torch.load(model_dir+ 'model_{}.pkl'.format(i)))
-                    print("loaded model {}".format(i))
-                    models["model{}".format(i)] = net
-                    del net
-                print("loaded trained models")
-            else:
-                print("models already loaded")
-            
-            egfr = predict(img, models)
-            print(egfr)        
 
-def predict(input_img, models):
+                    extract_CNN = nn.Sequential(*list(net.module.base_model.children())[:-1]).type(dtype)
+                    extract_net = torch.nn.DataParallel(extract_CNN)
+                    print("loaded model {}/10\r".format(i))
+                    models["model_{}".format(i)] = net
+                    extract_models["extract_model_{}".format(i)] = extract_net
+                    del net
+                print("loaded trained CNN models")
+            else:
+                print("CNN models already loaded")
+            
+            if 'xgb_models' not in locals():
+                print("loading trained XGBoost models...")
+
+                xgb_models = {}
+                xgb_dir = 'models/'
+                for i in range(10):
+                    xgb_model = pickle.load(open(xgb_dir + 'xgb_{}.pickle.dat'.format(i), "rb"))
+                    xgb_models["xgb_{}".format(i)] = xgb_model
+                print("loaded trained XGBoost models")
+            else:
+                print("XGBoost models already loaded")
+
+            egfr = predict_egfr(img, models)
+            print("Predictred eGFR: {}".format(egfr))
+            ckd_stage = predict_ckd(img, extract_models, xgb_models)
+            print("Predicted CKD stage < III: {:.2f}%".format(ckd_stage*100))
+                    
+
+def predict_egfr(input_img, models):
     pred = []
     
-    print("predicting...")    
+    print("predicting egfr...")    
     for i in range(10):
-        test_pred = models["model{}".format(i)](input_img)
+        test_pred = models["model_{}".format(i)].eval()(input_img)
         test_pred = test_pred.data.cpu().numpy()
         pred.append(test_pred)
-        print("prediction {}/10 completed".format(i+1))
+        print("prediction {}/10 completed".format(i+1), end="\r")
 
 
     ens_pred = sum(pred)/len(pred)
 
 
-    # In[ ]:
     return(ens_pred)
+
+def predict_ckd(input_img, extract_models, xgb_models):
+    pred = []
+
+    print("predicting CKD stage...")
+    for i in range(10):
+        test_features = extract_models["extract_model_{}".format(i)].eval()(input_img)
+        test_features = test_features.data.cpu().numpy()
+        test_features = pd.DataFrame(test_features.reshape(-1, 2048))
+        test_features.columns = [str(i) for i in range(2048)]
+        test_features = test_features[xgb_models["xgb_{}".format(i)].feature_names]
+        test_features = xgb.DMatrix(test_features)
+        test_class = xgb_models["xgb_{}".format(i)].predict(test_features)
+        pred.append(test_class)
+        print("prediction {}/10 completed".format(i+1), end="\r")
+
+    ens_pred = sum(pred)/len(pred)
+    # ens_pred = ens_pred > 0.5
+
+    return(ens_pred[0])
 
 if __name__ == '__main__':
     main()
